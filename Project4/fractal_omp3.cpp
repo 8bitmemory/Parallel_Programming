60bbea8aa4369ba8538637e8e8d026ba3c44f23c
Elliott <e_e102@txstate.edu>
2018-10-29 21:00:18 -0500

    removes the curly braces around the omp pragma

--- Project4/fractal_omp3.cpp
+++ Project4/fractal_omp3.cpp
@@ -33,7 +33,7 @@ static const double yMid =  0.55267;
 
 static void fractal(const int width, const int frames, unsigned char* pic)
 {
-# pragma omp parallel default(none) shared(Delta, xMid, yMid, pic, width, frames){
+# pragma omp parallel default(none) shared(Delta, xMid, yMid, pic, width, frames)
   // compute frames
   for (int frame = 0; frame < frames; frame++) {
     const double delta = Delta * pow(0.98, frame);
@@ -60,7 +60,6 @@ static void fractal(const int width, const int frames, unsigned char* pic)
       }
     }
   }
-}
 }
 
 int main(int argc, char *argv[])
