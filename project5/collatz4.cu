b4a7e6d9878478ad2d3567a2601e97ad3d62fdba
Elliott <e_e102@txstate.edu>
2018-11-15 02:14:19 -0600

    changes to what ive been doing

--- project5/collatz4.cu
+++ project5/collatz4.cu
@@ -35,7 +35,7 @@ static __global__ void collatzKernel(const long range, int *maxlen)
   const long idx = threadIdx.x + blockIdx.x * (long)blockDim.x;
   int my_beg = (idx * 4) + 1;
   int my_end = ((idx + 1) * 4) + 1;
-  //int localMax = 1;
+  int localMax = 0;
 
   if(idx < (range/4)){
     for(int i = my_beg; i < my_end; i++){
@@ -49,10 +49,9 @@ static __global__ void collatzKernel(const long range, int *maxlen)
           val = 3 * val + 1;  // odd
         }
       }
-      //if (localMax < len)localMax = len;
-      if (*maxlen < len) atomicMax(maxlen,len);
+      if (localMax < len)localMax = len;
     }
-    
+    if (*maxlen < localMax) atomicMax(maxlen, localMax);    
   }
 }
 
