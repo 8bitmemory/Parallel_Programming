7d2a29344e674f4bf90a3ba9942b3f3895680d63
Elliott <e_e102@txstate.edu>
2018-11-14 19:38:06 -0600

    removes h_range and d_range and fixes the kernel launch to divide range by 4

--- project5/collatz.cu
+++ project5/collatz.cu
@@ -29,14 +29,14 @@ Author: Martin Burtscher
 
 static const int ThreadsPerBlock = 512;
 
-static __global__ void collatzKernel(int * range, int * maxlen)
+static __global__ void collatzKernel(const long range, int * maxlen)
 {
   // compute sequence lengths
   const long idx = threadIdx.x + blockIdx.x * (long)blockDim.x;
   long val = idx+1;
   int len = 1;
 
-  if(idx < *range)
+  if(idx < range)
   while (val != 1) {
     len++;
     if ((val % 2) == 0) {
@@ -65,36 +65,31 @@ int main(int argc, char *argv[])
 
   // check command line
   if (argc != 2) {fprintf(stderr, "usage: %s range\n", argv[0]); exit(-1);}
-  const int range = atol(argv[1]);
+  const long range = atol(argv[1]);
   if (range < 1) {fprintf(stderr, "error: range must be at least 1\n"); exit(-1);}
   printf("range: 1, ..., %ld\n", range);
 
   
   // alloc space for device copies of maxlen and range
   int* d_maxlen;
-  int* d_range;
   const int size = sizeof(int);
   cudaMalloc((void **)&d_maxlen, size);
-  cudaMalloc((void **)&d_range, size);
 
 
   // alloc space for host copies of a, b, c and setup input values
   int* h_maxlen = new int;
-  const int* h_range = new int;
-  *d_maxlen = 0;
-  h_range = &range;
+  *h_maxlen = 0;
+
 
   // copy inputs to device
   if (cudaSuccess != cudaMemcpy(d_maxlen, h_maxlen, size, cudaMemcpyHostToDevice)) {fprintf(stderr, "copying to device failed\n"); exit(-1);};
-  if (cudaSuccess != cudaMemcpy(d_range, h_range, size, cudaMemcpyHostToDevice)) {fprintf(stderr, "copying to device failed\n"); exit(-1);};
-  
 
   // start time
   timeval start, end;
   gettimeofday(&start, NULL);
 
   //launch GPU kernel
-  collatzKernel<<<(ThreadsPerBlock + range - 1)/ThreadsPerBlock,ThreadsPerBlock>>>(d_range, d_maxlen);
+  collatzKernel<<<(ThreadsPerBlock + range - 1)/ThreadsPerBlock,ThreadsPerBlock>>>(range, d_maxlen);
   cudaDeviceSynchronize();
 
   // end time
@@ -111,9 +106,7 @@ int main(int argc, char *argv[])
 
   // clean up
   delete h_maxlen;
-  delete h_range;
   cudaFree(d_maxlen);
-  cudaFree(d_range);
 
   return 0;
 }
